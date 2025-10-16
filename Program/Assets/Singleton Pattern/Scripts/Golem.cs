32dee221b4411b1e3b00bc72beb8c56384c1e9a7
ysooj <musukie52@gmail.com>
2025-10-16 15:26:24 +0900

    [과제] (2025.10.16) Golem을 z축으로 전진후진 반복시키기

--- Program/Assets/Singleton Pattern/Scripts/Golem.cs
+++ Program/Assets/Singleton Pattern/Scripts/Golem.cs
@@ -2,8 +2,16 @@ using UnityEngine;
 
 public class Golem : Creature
 {
+    [SerializeField] Vector3 startPosition;
+
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
     public override void Behaviour()
     {
-        transform.
+        float offset = Mathf.PingPong(Time.time * speed, 5f);
+        transform.position = startPosition + new Vector3(0, 0, offset - 2.5f);
     }
 }
